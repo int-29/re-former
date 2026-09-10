a41d73e1934d2e833b207a2b404021ad44c4692c
int-29 <144659972+int-29@users.noreply.github.com>
2026-09-10 17:32:44 +0300

    do basic html form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+    def new
+    end
+    def create
+        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(allowed_user_params)
+        if @user.save
+        redirect_to new_user_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def allowed_user_params
+      params.expect(user: [ :username, :email, :password ])
+    end
+end
